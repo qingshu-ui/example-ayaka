f907869da397245a3de3660ebdd09165f844bbc3
qingshu-ui <zhenglantao@gmail.com>
2024-09-02 09:07:25 +0800

    fixed #2

--- src/main/kotlin/io/github/qingshu/ayaka/example/service/impl/QwenServiceImpl.kt
+++ src/main/kotlin/io/github/qingshu/ayaka/example/service/impl/QwenServiceImpl.kt
@@ -25,27 +25,37 @@ class QwenServiceImpl @Autowired constructor(
 
     private val userConversations = mutableMapOf<Long, MutableList<Message>>()
 
+    @Synchronized
     override fun chat(user: Long, message: String): String {
         val conversationHistory = userConversations.getOrPut(user) { mutableListOf() }
 
         if (conversationHistory.isEmpty() || conversationHistory.first() !is SystemMessage) {
             val systemMessage = SystemMessage("你是一名幽默的脱口秀老师，你应该以幽默风格回复任何问题。")
-            conversationHistory.add(systemMessage)
+            conversationHistory.add(0, systemMessage)
         }
         val userMessage = UserMessage(message)
         conversationHistory.add(userMessage)
 
-        val userMessageCount = conversationHistory.count { it !is SystemMessage }
-        if (userMessageCount > 10) {
-            val firstUserMessageIndex = conversationHistory.indexOfFirst { it !is SystemMessage }
-            conversationHistory.removeAt(firstUserMessageIndex)
+        while (conversationHistory.size > 11) {
+            val firstUserMessageIndex = conversationHistory.indexOfFirst { it is UserMessage || it is AssistantMessage }
+            if (firstUserMessageIndex != -1) {
+                if (firstUserMessageIndex < conversationHistory.size && conversationHistory[firstUserMessageIndex] is UserMessage && conversationHistory[firstUserMessageIndex + 1] is AssistantMessage) {
+                    conversationHistory.removeAt(firstUserMessageIndex + 1)
+                }
+                conversationHistory.removeAt(firstUserMessageIndex)
+            }
         }
 
         val prompt = Prompt(
             conversationHistory, ChatOptionsBuilder.builder().withModel("qwen-long").build()
         )
 
-        val resp = model.call(prompt).result.output.content
+        var resp = ""
+        try {
+            resp = model.call(prompt).result.output.content
+        } catch (_: Exception) {
+            println(prompt)
+        }
         conversationHistory.add(AssistantMessage(resp))
         return resp
     }
